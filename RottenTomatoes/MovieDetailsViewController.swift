726d4fb73d9736f508b3aa05f240ff8b2ace7037
Dan Hipschman <dsh@vicarious.com>
2015-02-08 12:10:59 -0800

    add scroll view for details

--- RottenTomatoes/MovieDetailsViewController.swift
+++ RottenTomatoes/MovieDetailsViewController.swift
@@ -12,14 +12,13 @@ class MovieDetailsViewController: UIViewController {
     
     var details: NSDictionary!
     @IBOutlet weak var posterView: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let tmbUrl = details.valueForKeyPath("posters.original") as String
         let url = tmbUrl.stringByReplacingOccurrencesOfString("_tmb.jpg", withString: "_ori.jpg")
         SVProgressHUD.show()
-        //posterView.setImageWithURL(NSURL(string: url))
         let request = NSURLRequest(URL: NSURL(string: url)!)
         posterView.setImageWithURLRequest(request, placeholderImage: nil,
             success: { request, response, image in
@@ -29,6 +28,33 @@ class MovieDetailsViewController: UIViewController {
                 SVProgressHUD.dismiss()
             }
         )
+        
+        addSynopsis()
+    }
+    
+    func addSynopsis() {
+        // it's much easier to set up scroll view programatically than in interface builder!
+        let scroll = UIScrollView(frame: view.frame)
+        view.addSubview(scroll)
+        
+        // set the frame width first, as sizeToFit will otherwise put everything on one line
+        let labelFrame = CGRectMake(view.frame.origin.x + 8, view.frame.origin.y + 8, view.frame.width - 8, 9999)
+        let label = UILabel(frame: labelFrame)
+        label.numberOfLines = 0
+        label.textColor = UIColor(white: 230, alpha: 1)
+        label.text = details["synopsis"] as? String
+        label.sizeToFit()
+        
+        // need to add uilabel to a content view so it goes at the top.  otherwise, the text will
+        // float in the vertical center
+        let height = view.frame.height / 6 + label.frame.height
+        var rect = CGRectMake(view.frame.origin.x, view.frame.origin.y, view.frame.width, height)
+        rect.origin.y += 5 * view.frame.height / 6
+        let contentView = UIView(frame: rect)
+        contentView.backgroundColor = UIColor(white: 0, alpha: 0.7)
+        contentView.addSubview(label)
+        scroll.addSubview(contentView)
+        scroll.contentSize = CGSizeMake(rect.width, rect.origin.y + rect.height)
     }
 
     override func didReceiveMemoryWarning() {
